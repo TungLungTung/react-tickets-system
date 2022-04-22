e90b3b6ec038181df33f0040be7f8ce05dc3b0e2
Tung Le <tunglungtung87@gmail.com>
2022-04-22 14:26:18 +0700

    finished this projects

--- backend/server.js
+++ backend/server.js
@@ -17,21 +17,28 @@ app.use(express.urlencoded({ extended: false }));
 /// Middleware - handler Error handler
 const { errorHandler } = require('./middleware/errorMiddleware.js');
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Welcome to Support Desk API'
-  });
-});
-
 /// ROUTES
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
-app.use(errorHandler);
 
 /// Server FrontEnd
 if (process.env.NODE_ENV === 'production') {
+  /// Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  /// accept for anything rou
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({
+      message: 'Welcome to Support Desk API'
+    });
+  });
 }
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
